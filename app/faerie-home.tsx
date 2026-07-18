2d780370420e2b4d8bf30d032f454b6f68887dc4
Vũ Thế Anh <112144434+Vutheanh11@users.noreply.github.com>
2026-07-18 16:36:28 +0700

    Add Faerie address

--- app/faerie-home.tsx
+++ app/faerie-home.tsx
@@ -880,6 +880,7 @@ export default function Home() {
           <span>CONTACT // FAERIE</span>
           <a href="mailto:faeriesolace@gmail.com">faeriesolace@gmail.com</a>
           <a href="https://www.facebook.com/profile.php?id=61577779404694" target="_blank" rel="noreferrer">Facebook Faerie ↗</a>
+          <address>Lô E2a-7, Đường D1 Khu Công nghệ cao, P. Long Thạnh Mỹ, TP. Thủ Đức, Ho Chi Minh City, Vietnam</address>
         </div>
         <button onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>Lên đầu trang ↑</button>
       </footer>
